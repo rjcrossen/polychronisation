394969f3d8391c91656f417301c57aba7b7aa5b1
rjcrossen <rjcrossen@outlook.com>
2024-11-13 17:00:41 +0000

    add sequential_spnet script

---
+++ sequential_spnet.m
@@ -0,0 +1,6 @@
+% Runs neural networks with spike-timing dependent plasticity and
+% conduction delays sequentially and saves information to a file to gain
+% information about polychronous groups.
+% Adapted from Eugene M. Izhikevich's spnet.m script (2004).
+% Adapted/written by Robert J. Crossen (November, 2024).
+
